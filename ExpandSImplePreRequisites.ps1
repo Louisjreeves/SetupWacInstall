0e74c9a6cc2b624223586d36d089c4e1a74dacea
Louisjreeves <79279019+Louisjreeves@users.noreply.github.com>
2022-05-27 12:27:44 -0500

    Update ExpandSImplePreRequisites.ps1

--- ExpandSImplePreRequisites.ps1
+++ ExpandSImplePreRequisites.ps1
@@ -3,10 +3,9 @@
 $ans1 = Read-host "Would you like to download and run or do you have the file pre-downloaded? 1=Proceed online 2=I have the file "
 
 If ($ans1 -ieq "1") { 
-set-location $env:USERPROFILE
- $myloc = $env:USERPROFILE
-$MyTemp=(Get-Item $myloc).fullname
-$mydownloads= (New-Object -ComObject Shell.Application).NameSpace('shell:$mytemp').Self.Path
+
+$MyTemp=(Get-Item $env:temp).fullname
+$mydownloads= (New-Object -ComObject Shell.Application).NameSpace('shell:Downloads').Self.Path
 $myuser= $env:USERNAME
  
 try
@@ -23,10 +22,28 @@ try
  
  
  $activedirectory= "C:\Users\$myuser\RCSimplePreRequsites"
- set-location "c:\"
+ cd c:\
  set-location $activedirectory
   .\RCSimplePreRequsites.ps1
 
+ 
+ #OLD --------------------LEGACY---------------------------
+ # $fullpath= "C:\Users\*\AppData\Local\Temp\ProSupportNTWKTtest.zip"
+
+#$GetFile = $mytemp # diag-v-master.zip
+#$getDIagVFol = (split-path $fullpath -Leaf).Split(".")[0]  #diag-v-master
+#$getDiagpath = (split-path $fullpath -parent)  #C:\Users\Administrator.Corp\Desktop
+
+#$md = "$env:ProgramFiles\WindowsPowerShell\Modules"
+#$final= "c:\ProgramFiles\WindowsPowerShell\Modules\Diag-V-master\src\"
+#$final2= "c:\Program Files\WindowsPowerShell\Modules\Diag-V-master\src\diag-v"
+#$extractedDiag= "Diag-v-master"
+#$finalFile= "diag-V"
+#Expand-Archive -Path $getdiagpath\$getDIagVFol.zip -DestinationPath $md -Force
+#cp -Recurse $getfile $md -Force -ErrorAction Stop
+#cp -Recurse $final2 $md -Force -ErrorAction stop
+#Import-module -Verbose $md\$finalfile -Force
+#start-sleep 5
  
  #OLD --------------------LEGACY---------------------------
  # $fullpath= "C:\Users\*\AppData\Local\Temp\ProSupportNTWKTtest.zip"
